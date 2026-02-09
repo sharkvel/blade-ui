8d6680d57cf0615371d12af4f5e14dea6cbabdd6
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-02-09 16:11:35 +0530

    Migrate label

--- resources/views/components/ui/label.blade.php
+++ resources/views/components/ui/label.blade.php
@@ -1,1 +1,22 @@
-<label {{ $attributes }}>{{ $slot }}</label>
+@php
+    /**
+     * Base classes
+     */
+    $baseClasses = "text-sm leading-none font-medium select-none disabled:pointer-events-none disabled:opacity-50";
+
+    /**
+     * Variant classes
+     */
+    $variantClasses = match ($variant) {
+        "default" => "text-foreground",
+    };
+
+    /**
+     * Size classes
+     */
+    $sizeClasses = match ($size) {
+        "default" => "text-sm",
+    };
+@endphp
+
+<label {{ $attributes->twMerge($baseClasses, $variantClasses, $sizeClasses) }}>{{ $slot }}</label>
