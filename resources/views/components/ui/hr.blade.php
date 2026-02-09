2b904792d01b2beb18e0b0a43e83887dba8d43de
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-02-09 15:55:02 +0530

    Migrate separator

--- resources/views/components/ui/hr.blade.php
+++ resources/views/components/ui/hr.blade.php
@@ -1,1 +1,8 @@
-<hr {{ $attributes }} />
+@php
+    /**
+     * Base Classes
+     */
+    $baseClasses = "my-6";
+@endphp
+
+<hr {{ $attributes->twMerge($baseClasses) }} />
--- resources/views/components/ui/vr.blade.php
+++ resources/views/components/ui/vr.blade.php
@@ -1,1 +1,8 @@
-<vr {{ $attributes }}></vr>
+@php
+    /**
+     * Base Classes
+     */
+    $baseClasses = "mx-6 h-full border-r";
+@endphp
+
+<vr {{ $attributes->twMerge($baseClasses) }}></vr>
