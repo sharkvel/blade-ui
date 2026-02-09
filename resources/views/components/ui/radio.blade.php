8001d905400b483ee82c9883a16f6a11b7074e77
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-02-09 16:16:51 +0530

    Migrate radio

--- resources/views/components/ui/radio.blade.php
+++ resources/views/components/ui/radio.blade.php
@@ -1,1 +1,35 @@
-<input type="radio" {{ $attributes }} />
+@props([
+    "variant" => "default",
+    "size" => "default",
+])
+
+@php
+    /**
+     * Base classes
+     */
+    $baseClasses = "checked:after:content flex appearance-none items-center justify-center rounded-full border border-transparent after:rounded-full disabled:pointer-events-none disabled:opacity-50";
+
+    /**
+     * Variant classes
+     */
+    $variantClasses = match ($variant) {
+        "default" => "border-input shadow-xs checked:border-primary checked:bg-primary checked:after:bg-primary-foreground dark:bg-input/30 dark:hover:not-checked:bg-input/50",
+        "secondary" => "bg-secondary checked:after:bg-primary",
+        "outline" => "border-input shadow-xs checked:after:bg-primary dark:bg-input/30 dark:hover:not-checked:bg-input/50",
+        "ghost" => "bg-transparent checked:after:bg-primary",
+    };
+
+    /**
+     * Size classes
+     */
+    $sizeClasses = match ($size) {
+        "sm" => "size-3 after:size-1.5",
+        "default" => "size-4 after:size-2",
+        "lg" => "size-5 after:size-2.5",
+    };
+@endphp
+
+<input
+    type="radio"
+    {{ $attributes->twMerge($baseClasses, $variantClasses, $sizeClasses) }}
+/>
