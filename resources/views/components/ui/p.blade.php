ce4ff7bc1acb50b6f7b6618817df0761b0161754
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-02-09 19:12:18 +0530

    Update p.blade.php

--- resources/views/components/ui/p.blade.php
+++ resources/views/components/ui/p.blade.php
@@ -2,7 +2,8 @@
     /**
      * Base Classes
      */
-    $baseClasses = "text-base leading-relaxed";
+    $notFirst = preg_match("/\b(mt|my)-\S*/", $attributes["class"] ?? "") ? "" : "not-first:mt-6";
+    $baseClasses = "text-base leading-relaxed $notFirst";
 @endphp
 
 <p {{ $attributes->twMerge($baseClasses) }}>{{ $slot }}</p>
